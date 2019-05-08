ab9f832ac6f2a015eac53bede0d6c90bc54b12d9
rajesh rao <rajesh.rao@icemobile.com>
2019-05-08 17:21:49 +0200

    Added test data in en locale

--- Tests/Fakery/Generators/CurrencySpec.swift
+++ Tests/Fakery/Generators/CurrencySpec.swift
@@ -6,16 +6,14 @@ final class CurrencySpec: QuickSpec {
   override func spec() {
     describe("Address") {
       var currency: Currency!
-     
       beforeEach {
         let parser = Parser(locale: "en-TEST")
         currency = Currency(parser: parser)
       }
-      
       describe("#currencyCode") {
         it("returns the correct text") {
           let code = currency.code()
-          expect(code).to(equal("EUR"))
+          expect(code).to(match("^[A-Za-z]{3}$"))
         }
       }
     }
